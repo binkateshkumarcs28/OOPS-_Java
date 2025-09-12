6680cd4c8f7bbf0f63cc1b58b05b240eb6bbb78e
binkateshkumarcs28 <binkatesh.kumar.cs28@iilm.edu>
2025-09-12 19:56:28 +0530

    Add files via upload

---
+++ address.java
@@ -0,0 +1,14 @@
+public class address {
+    public static void main(String[] args) {
+        // Method 1: Using Single println
+        System.out.println("---- Using Single println ----");
+        System.out.println("Name: Binkatesh Kumar, Address: B-117 P.C. Colony, Patna, Email: binkatesh.kumar.cs28@iilm.edu, Contact: 8547893246");
+
+        // Method 2: Using Multiple println
+        System.out.println("\n---- Using Multiple println ----");
+        System.out.println("Name: Binkatesh Kumar");
+        System.out.println("Address: B-117 P.C. Colony, Patna");
+        System.out.println("Email: binkatesh.kumar.cs28@iilm.edu");
+        System.out.println("Contact: 95647158934");
+    }
+}
