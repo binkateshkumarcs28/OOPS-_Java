e2f0669cb0662f8f3fa74abc43bee9ed2bf3d2fb
binkateshkumarcs28 <binkatesh.kumar.cs28@iilm.edu>
2025-09-19 12:11:45 +0530

    Add files via upload

---
+++ static_method.java
@@ -0,0 +1,27 @@
+public class static_method {
+    // static variable
+     static int a = 40;
+    // instance variable
+    int b = 50;
+
+    void simpleDisplay()
+    {
+        System.out.println(a);
+        System.out.println(b);
+    }
+    // Declaration of a static method
+    static void staticDisplay()
+    { 
+      System.out.println(a); 
+    }
+    // main method
+    public static void main(String[] args)
+    {
+        static_method obj = new static_method ();
+        obj.simpleDisplay();
+
+        // Calling static method
+        static_method.staticDisplay();
+        staticDisplay();
+    }
+}
