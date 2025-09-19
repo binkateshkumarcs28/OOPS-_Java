2a950750519bcc459639e207cf7d2a5815a9645e
binkateshkumarcs28 <binkatesh.kumar.cs28@iilm.edu>
2025-09-19 11:36:56 +0530

    Add files via upload

---
+++ exception_handling.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class exception_handling {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.print("First Number: ");
+        a = sc.nextInt();
+
+        System.out.print("Second Number: ");
+        b = sc.nextInt();
+
+        c = a / b;
+
+        System.out.println("Result = " + c);
+        System.out.println("Some other work");
+}
+}
